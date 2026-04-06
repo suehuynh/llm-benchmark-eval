e157af1ecf824150e4f37472ee779e0639870050
Sue Huynh <58705955+suehuynh@users.noreply.github.com>
2026-04-06 02:51:04 +0000

    chore: initialize project environment, licensing, and package metadata

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import find_packages, setup
 from typing import List
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="anime-recsys",
     version="0.0.1",
     author="SueHuynh",
     packages= find_packages(),
-    install_requires = []
+    install_requires=requirements
 )
